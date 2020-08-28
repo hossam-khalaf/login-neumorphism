a1314e8d8a9985ec0d3ed9411eb54f5af94d6941
hossam-khalaf <hossamhassan303089@gmail.com>
2020-08-28 22:11:08 +0200

    Adding dark mood

---
+++ app.js
@@ -0,0 +1,14 @@
+let toggleBtn = document.querySelector('#toggleBtn');
+
+let bodyEl = document.querySelector('body');
+
+let darkMode = false;
+
+toggleBtn.addEventListener('change', (event) => {
+  darkMode = event.target.checked;
+  if (darkMode) {
+    bodyEl.classList.add('dark');
+  } else {
+    bodyEl.classList.remove('dark');
+  }
+});
